6aa2a3a0361e33722fbd07954f63ad26bcfd93b4
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2021-04-22 20:48:44 -0300

    Mandando o bot iniciar

--- Mr_Prof_Foca.py
+++ Mr_Prof_Foca.py
@@ -1,22 +1,31 @@
 import discord
 import os
 from dotenv import load_dotenv
+from discord.ext import commands
+
+
 
 
 load_dotenv()
 
-client = discord.Client()
+#client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
-@client.event
+#@client.event
+@bot.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
+    print('We have logged in as {}'.format(bot.user))
 
-@client.event
-async def on_message(message):
+#@client.event
+@bot.command(name="start", help="Inicia o foco ")
+async def start_timer(ctx):
+    await ctx.send("Hora de focar!")
+    """
     if message.author == client.user:
         return
-
     if message.content.startswith('$hello'):
         await message.channel.send('Hello!')
+    """
 
-client.run(os.environ['BOT_TOKEN'])
+#client.run(os.environ['BOT_TOKEN'])
+bot.run(os.environ['BOT_TOKEN'])

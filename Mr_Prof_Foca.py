c22f3ee9bbba5536545a8ea9c460901c0e1721a5
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2021-04-22 21:23:31 -0300

    add a timer formatting simple

--- Mr_Prof_Foca.py
+++ Mr_Prof_Foca.py
@@ -1,7 +1,9 @@
+import asyncio
 import discord
 import os
 from dotenv import load_dotenv
 from discord.ext import commands
+
 
 
 
@@ -19,13 +21,16 @@ async def on_ready():
 #@client.event
 @bot.command(name="start", help="Inicia o foco ")
 async def start_timer(ctx):
-    await ctx.send("Hora de focar!")
-    """
-    if message.author == client.user:
-        return
-    if message.content.startswith('$hello'):
-        await message.channel.send('Hello!')
-    """
+    start_work_em = discord.Embed(title= "Hora de começar a focar!", color=0x33c633)
+    await ctx.send(embed = start_work_em)
+    await asyncio.sleep(5)
+    start_play_em = discord.Embed(title="Hora de começar sua pausa!", color=0x33c633)
+    await ctx.send(embed = start_play_em)
+
+@bot.command(name="stop", help="Para o foco ")
+async def stop_timer(ctx):
+    stop_timer_em = discord.Embed(title= "Hora de tirar uma pausa!", color=0x633333)
+    await ctx.send(embed = stop_timer_em)
 
 #client.run(os.environ['BOT_TOKEN'])
 bot.run(os.environ['BOT_TOKEN'])

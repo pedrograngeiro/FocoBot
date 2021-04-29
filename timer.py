5b258bf2e617c5bc9b745c8513b61ea3522275ea
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2021-04-29 16:26:43 -0300

    Melhoria na definição do status do tempo

--- timer.py
+++ timer.py
@@ -1,18 +1,28 @@
+
+from enum import Enum
+
+class TimerStatus(Enum):
+    INITIALIZED = 1
+    RUNNING = 2
+    STOPPED = 3
+    EXPIRED = 4
+
 class Timer:
-    def __init__(self):
-        self.running = False
+    def __init__(self, max_ticks = 10):
+        self.status = TimerStatus.INITIALIZED
         self.ticks = 0
-        self.max_ticks = 5  # 25 * 60
+        self.max_ticks = max_ticks  # 25 * 60
+
+    def get_status(self):
+        return self.status
+
 
     def start(self):
-        self.running = True
+        self.status = TimerStatus.RUNNING
         self.ticks = 0
 
     def stop(self):
-        self.running = False
-
-    def is_running(self):
-        return self.running
+        self.status = TimerStatus.STOPPED
 
     def get_ticks(self):
         return self.ticks
@@ -20,5 +30,5 @@ class Timer:
     def tick(self):
         self.ticks += 1
         if self.get_ticks() >= self.max_ticks:
-            self.stop()
+            self.status = TimerStatus.EXPIRED
 

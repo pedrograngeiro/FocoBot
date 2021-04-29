0fe0070ced17a760b7d5070000d45dd3c4b73a0d
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2021-04-29 16:26:10 -0300

    Novos teste no Timer e no TimerStatus

--- tests/timer_test.py
+++ tests/timer_test.py
@@ -1,21 +1,32 @@
-from timer import Timer
+from timer import Timer, TimerStatus
+
+def test_init_initializes_vars_properly():
+    timer = Timer(max_ticks = 1)
+    assert timer.get_status() == TimerStatus.INITIALIZED
+
+    assert timer.get_ticks() == 0
 
 
 def test_start_initializes_vars_properly():
     timer = Timer()
     timer.start()
-    assert timer.is_running()
+    assert timer.get_status() == TimerStatus.RUNNING
     assert timer.get_ticks() == 0
 
-
-def test_init_initializes_vars_properly():
-    timer = Timer()
-    assert not timer.is_running()
-    assert timer.get_ticks() == 0
 
 def test_tick_increases_ticks():
-    timer = Timer()
+    timer = Timer(max_ticks=2)
     timer.start()
     timer.tick()
-    assert timer.is_running()
+    assert timer.get_status() == TimerStatus.RUNNING
     assert timer.get_ticks() == 1
+
+def test_tick_will_expire_when_it_reaches_max_ticks():
+    timer = Timer(max_ticks=2)
+    timer.start()
+    timer.tick()
+    assert timer.get_status() == TimerStatus.RUNNING
+    assert timer.get_ticks() == 1
+    timer.tick()
+    assert timer.get_status() == TimerStatus.EXPIRED
+    assert timer.get_ticks() == 2

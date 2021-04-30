dd85ba9afdf29eb4618ac3bd63ff502e0fda2f04
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2021-04-30 12:20:41 -0300

    Tradução de funções e atualização do status tempo

--- discord_cog.py
+++ discord_cog.py
@@ -20,22 +20,52 @@ class DiscordCog(commands.Cog):
 
     @commands.command()
     async def start(self, ctx):
-        if self.timer.get_status() == TimerStatus.RUNNING:
+        if self.timer.get_status() == TimerStatus.RODANDO:
             await self.show_message(ctx, "O bot de foco já esta rodando! ", COLOR_SUCCESS)
             return
         await self.show_message(ctx, "Hora de começar a focar! ", COLOR_SUCCESS)
-        self.timer.start(max_ticks=10)
-        while self.timer.get_status() == TimerStatus.RUNNING:
-            await asyncio.sleep(1)  # 25 x 60
+
+        #Round 1
+        self.timer.start(max_ticks=1.500) #25 minutos
+        while self.timer.get_status() == TimerStatus.RODANDO:
+            await asyncio.sleep(1)
             self.timer.tick()
-        if self.timer.get_status() == TimerStatus.EXPIRED:
+        if self.timer.get_status() == TimerStatus.FINALIZADO:
             await self.show_message(ctx, "Hora de descansar! ", COLOR_SUCCESS)
-            self.timer.start(max_ticks=10)
-            while self.timer.get_status() == TimerStatus.RUNNING:
+            self.timer.start(max_ticks=300)
+            while self.timer.get_status() == TimerStatus.RODANDO:
                 await asyncio.sleep(1)  # 25 x 60
                 self.timer.tick()
-            if self.timer.get_status() == TimerStatus.EXPIRED:
-                await self.show_message(ctx, "Hora de uma pausa longa! ", COLOR_SUCCESS)
+            if self.timer.get_status() == TimerStatus.FINALIZADO:
+                await self.show_message(ctx, "Hora de começar a focar ", COLOR_SUCCESS)
+
+                # Round 2
+                self.timer.start(max_ticks=1.500)  #25 minutos
+                while self.timer.get_status() == TimerStatus.RODANDO:
+                    await asyncio.sleep(1)
+                    self.timer.tick()
+                if self.timer.get_status() == TimerStatus.FINALIZADO:
+                    await self.show_message(ctx, "Hora de descansar! ", COLOR_SUCCESS)
+                    self.timer.start(max_ticks=300)
+                    while self.timer.get_status() == TimerStatus.RODANDO:
+                        await asyncio.sleep(1)  # 25 x 60
+                        self.timer.tick()
+                    if self.timer.get_status() == TimerStatus.FINALIZADO:
+                        await self.show_message(ctx, "Hora de começar a focar ", COLOR_SUCCESS)
+
+                        # Round 3
+                        self.timer.start(max_ticks=1.500)  #25 minutos
+                        while self.timer.get_status() == TimerStatus.RODANDO:
+                            await asyncio.sleep(1)
+                            self.timer.tick()
+                        if self.timer.get_status() == TimerStatus.FINALIZADO:
+                            await self.show_message(ctx, "Hora de descansar! ", COLOR_SUCCESS)
+                            self.timer.start(max_ticks=300)
+                            while self.timer.get_status() == TimerStatus.RODANDO:
+                                await asyncio.sleep(1)  # 25 x 60
+                                self.timer.tick()
+                            if self.timer.get_status() == TimerStatus.FINALIZADO:
+                                await self.show_message(ctx, "Hora de começar a focar ", COLOR_SUCCESS)
 
     async def show_message(self, ctx, title, color):
         start_work_em = discord.Embed(title=title, color=color)
@@ -43,16 +73,28 @@ class DiscordCog(commands.Cog):
 
     @commands.command()
     async def stop(self, ctx):
-        if self.timer.get_status() != TimerStatus.RUNNING:
+        if self.timer.get_status() != TimerStatus.RODANDO:
             await self.show_message(ctx, "O bot já está pausado! ", COLOR_SUCCESS)
             return
         await self.show_message(ctx, "Hora de fazer uma pausa!", COLOR_DANGER)
         self.timer.stop()
 
     @commands.command()
-    async def show_time(self, ctx):
-        await ctx.send(f"Current time status is: {self.timer.get_status()}")
-        await ctx.send(f"Current time is: {self.timer.get_ticks()}")
+    async def mostrar_tempo(self, ctx):
+        if self.timer.get_status() == TimerStatus.INICIALIZADO:
+            self.tempo = 'ONLINE'
+
+        if self.timer.get_status() == TimerStatus.RODANDO:
+            self.tempo = 'RODANDO'
+
+        if self.timer.get_status() == TimerStatus.PARADO:
+            self.tempo = 'PARADO'
+
+        if self.timer.get_status() == TimerStatus.FINALIZADO:
+            self.tempo = 'FINALIZADO'
+
+        await ctx.send(f"O bot está: {self.tempo}")
+        await ctx.send(f"O tempo é: {self.timer.get_ticks()}")
 
     @commands.command()
     async def show_help(self, ctx):
--- timer.py
+++ timer.py
@@ -1,28 +1,26 @@
-
 from enum import Enum
 
 class TimerStatus(Enum):
-    INITIALIZED = 1
-    RUNNING = 2
-    STOPPED = 3
-    EXPIRED = 4
+    INICIALIZADO = 1
+    RODANDO = 2
+    PARADO = 3
+    FINALIZADO = 4
 
 class Timer:
     def __init__(self):
-        self.status = TimerStatus.INITIALIZED
+        self.status = TimerStatus.INICIALIZADO
         self.ticks = 0
 
     def get_status(self):
         return self.status
 
-
     def start(self, max_ticks):
         self.max_ticks = max_ticks
-        self.status = TimerStatus.RUNNING
+        self.status = TimerStatus.RODANDO
         self.ticks = 0
 
     def stop(self):
-        self.status = TimerStatus.STOPPED
+        self.status = TimerStatus.PARADO
 
     def get_ticks(self):
         return self.ticks
@@ -30,5 +28,5 @@ class Timer:
     def tick(self):
         self.ticks += 1
         if self.get_ticks() >= self.max_ticks:
-            self.status = TimerStatus.EXPIRED
+            self.status = TimerStatus.FINALIZADO
 

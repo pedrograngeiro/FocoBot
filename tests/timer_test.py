4e0900125604bae08550aa7d715c35fd25a7e197
Pedro Grangeiro <46334439+pedrograngeiro@users.noreply.github.com>
2021-04-30 12:20:56 -0300

    Tradução das funções

--- tests/timer_test.py
+++ tests/timer_test.py
@@ -2,7 +2,7 @@ from timer import Timer, TimerStatus
 
 def test_init_initializes_vars_properly():
     timer = Timer()
-    assert timer.get_status() == TimerStatus.INITIALIZED
+    assert timer.get_status() == TimerStatus.INICIALIZADO
 
     assert timer.get_ticks() == 0
 
@@ -10,7 +10,7 @@ def test_init_initializes_vars_properly():
 def test_start_initializes_vars_properly():
     timer = Timer()
     timer.start(max_ticks=0)
-    assert timer.get_status() == TimerStatus.RUNNING
+    assert timer.get_status() == TimerStatus.RODANDO
     assert timer.get_ticks() == 0
 
 
@@ -18,15 +18,15 @@ def test_tick_increases_ticks():
     timer = Timer(max_ticks=2)
     timer.start()
     timer.tick()
-    assert timer.get_status() == TimerStatus.RUNNING
+    assert timer.get_status() == TimerStatus.RODANDO
     assert timer.get_ticks() == 1
 
 def test_tick_will_expire_when_it_reaches_max_ticks():
     timer = Timer(max_ticks=2)
     timer.start()
     timer.tick()
-    assert timer.get_status() == TimerStatus.RUNNING
+    assert timer.get_status() == TimerStatus.RODANDO
     assert timer.get_ticks() == 1
     timer.tick()
-    assert timer.get_status() == TimerStatus.EXPIRED
+    assert timer.get_status() == TimerStatus.FINALIZADO
     assert timer.get_ticks() == 2
